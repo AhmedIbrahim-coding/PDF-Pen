5e1f6477b55808bfb7d84c355da2d2a13a4cf96b
AhmedIbrahim-coding <ahmed.ibrahim.eladawy7@gmail.com>
2025-12-15 13:00:54 +0200

    Complete displaying files

--- UI.py
+++ UI.py
@@ -1,6 +1,7 @@
 import customtkinter as ctk
 from tkinter import filedialog
 from PIL import Image
+import os
 
 class App(ctk.CTk):
     def __init__(self):
@@ -17,7 +18,7 @@ class App(ctk.CTk):
 
 
         # prepare the browse image
-        pil_drop_icon = Image.open("Assets\\export.png")
+        pil_drop_icon = Image.open("Assets\\import.png")
         drop_icon = ctk.CTkImage(light_image=pil_drop_icon,
                                  dark_image=pil_drop_icon,
                                  size=(150,150))
@@ -44,7 +45,8 @@ class App(ctk.CTk):
                                      font=ctk.CTkFont(family="Arial", size=20, weight="bold"),
                                      state="disabled",
                                      fg_color="#18185F",
-                                     text_color="#A1A1A1")
+                                     text_color="#A1A1A1",
+                                     command=self.merging)
         self.merge_button.place(y=400, x=180, anchor="center")
 
         # Split
@@ -77,9 +79,50 @@ class App(ctk.CTk):
                 self.files_field.destroy()
                 self.files_field = ctk.CTkScrollableFrame(self, width=700, height=290, fg_color="#2E2E2E")
                 self.files_field.pack(padx = 10, pady =10)
+
+                # add an icon for the adding button
+                pil_add_icon = Image.open("Assets\\plus.png")
+                add_icon = ctk.CTkImage(light_image=pil_add_icon,
+                                        dark_image=pil_add_icon,
+                                        size=(30,30))
+                
+                # add a button once to add more files, but only once if the files list is impty
+                self.adding_button = ctk.CTkButton(self.files_field,
+                                                   height=50,
+                                                   width=50,
+                                                   text="",
+                                                   fg_color="#3050b9",
+                                                   hover_color="#24508A",
+                                                   image=add_icon, command=self.Browse)
+                self.adding_button.pack(pady=3, padx=5, anchor="w")
+                
+
             # add the selected files to the files list
             self.files_list.extend(files)
+            self.show_added_files(files)
 
     def enable_buttons(self):
         for btn in (self.split_button, self.merge_button, self.compress_button):
             btn.configure(state="normal", fg_color="#2C2AB4", hover_color="#1e1cb1", text_color="white")
+
+    def show_added_files(self, files_list):
+        # add all the selected files to the files field
+        for file in files_list:
+            file_label = ctk.CTkFrame(self.files_field, width=400, height=50, fg_color="#254661")
+            file_label.pack(pady=3, padx=5, anchor="w", fill="x", before=self.adding_button) # add the labels before the add button
+
+            # display file's name
+            file_fullname = os.path.basename(file)
+            max_name_len = 15
+            file_name = f"{file_fullname[:max_name_len+1]}..." if len(file_fullname) > max_name_len else file_fullname
+            file_name_label = ctk.CTkLabel(file_label, text=file_name, font=ctk.CTkFont(family="Arial", size=15, weight="bold"))
+            file_name_label.place(rely=0.5, relx=0, x=10, anchor="w")
+
+    def merging(self):
+        self.merge_window = ctk.CTkToplevel(self)
+        self.merge_window.geometry("960x540")
+        self.title("Merge")
+        self.merge_window.resizable(False, False)
+        self.merge_window.grab_set()
+        self.merge_window.focus()
+        
